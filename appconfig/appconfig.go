08a6f68b55595171d9086f55f8af360e87cb0cb2
Chen Asraf <casraf@pm.me>
2025-01-15 01:08:03 +0200

    refactor(appconfig): extract installer to file

--- appconfig/appconfig.go
+++ appconfig/appconfig.go
@@ -9,7 +9,6 @@ import (
 	"strings"
 
 	"github.com/chenasraf/sofmani/logger"
-	"github.com/chenasraf/sofmani/platform"
 	"github.com/eschao/config"
 )
 
@@ -33,55 +32,12 @@ type AppConfigDefaults struct {
 	Type *map[InstallerType]Installer `json:"type" yaml:"type"`
 }
 
-type Installer struct {
-	Name           *string                       `json:"name"              yaml:"name"`
-	Type           InstallerType                 `json:"type"              yaml:"type"`
-	Env            *map[string]string            `json:"env"               yaml:"env"`
-	Platforms      *platform.Platforms           `json:"platforms"         yaml:"platforms"`
-	Steps          *[]Installer                  `json:"steps"             yaml:"steps"`
-	Opts           *map[string]any               `json:"opts"              yaml:"opts"`
-	BinName        *string                       `json:"bin_name"          yaml:"bin_name"`
-	CheckHasUpdate *string                       `json:"check_has_update"  yaml:"check_has_update"`
-	CheckInstalled *string                       `json:"check_installed"   yaml:"check_installed"`
-	PostInstall    *string                       `json:"post_install"      yaml:"post_install"`
-	PreInstall     *string                       `json:"pre_install"       yaml:"pre_install"`
-	PostUpdate     *string                       `json:"post_update"       yaml:"post_update"`
-	PreUpdate      *string                       `json:"pre_update"        yaml:"pre_update"`
-	EnvShell       *platform.PlatformMap[string] `json:"env_shell"         yaml:"env_shell"`
-}
-
-type InstallerType string
-
-const (
-	InstallerTypeGroup    InstallerType = "group"
-	InstallerTypeShell    InstallerType = "shell"
-	InstallerTypeBrew     InstallerType = "brew"
-	InstallerTypeApt      InstallerType = "apt"
-	InstallerTypeGit      InstallerType = "git"
-	InstallerTypeRsync    InstallerType = "rsync"
-	InstallerTypeNpm      InstallerType = "npm"
-	InstallerTypePnpm     InstallerType = "pnpm"
-	InstallerTypeYarn     InstallerType = "yarn"
-	InstallerTypeManifest InstallerType = "manifest"
-)
-
 func (c *AppConfig) Environ() []string {
 	if c.Env == nil {
 		return []string{}
 	}
 	out := []string{}
 	for k, v := range *c.Env {
-		out = append(out, fmt.Sprintf("%s=%s", k, v))
-	}
-	return out
-}
-
-func (i *Installer) Environ() []string {
-	if i.Env == nil {
-		return []string{}
-	}
-	out := []string{}
-	for k, v := range *i.Env {
 		out = append(out, fmt.Sprintf("%s=%s", k, v))
 	}
 	return out
