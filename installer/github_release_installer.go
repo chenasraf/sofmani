688ddf6cfe994aa49227b292ff9fdfb43260f130
Chen Asraf <casraf@pm.me>
2025-01-23 02:13:17 +0200

    WIP

--- installer/github_release_installer.go
+++ installer/github_release_installer.go
@@ -2,6 +2,7 @@ package installer
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -25,13 +26,29 @@ type GitHubReleaseOpts struct {
 	Destination               *string
 	DownloadFilename          *string
 	PlatformDownloadFilenames *platform.PlatformMap[string]
+	Strategy                  *GitHubReleaseInstallStrategy
 }
+
+type GitHubReleaseInstallStrategy string
+
+const (
+	StrategyNone GitHubReleaseInstallStrategy = "none"
+	StrategyTar  GitHubReleaseInstallStrategy = "tar"
+	StrategyZip  GitHubReleaseInstallStrategy = "zip"
+)
 
 // Install implements IInstaller.
 func (i *GitHubReleaseInstaller) Install() error {
 	opts := i.GetOpts()
+	data := i.GetData()
+	name := *data.Name
+	tmpDir, err := os.MkdirTemp("", "sofmani")
+	if err != nil {
+		return err
+	}
+	tmpOut, err := os.Create(fmt.Sprintf("%s/%s", tmpDir, name))
 
-	out, err := os.Create(fmt.Sprintf("%s/%s", *opts.Destination, *i.GetData().Name))
+	out, err := os.Create(fmt.Sprintf("%s/%s", *opts.Destination, name))
 	defer out.Close()
 	if err != nil {
 		return err
@@ -43,6 +60,9 @@ func (i *GitHubReleaseInstaller) Install() error {
 	}
 
 	filename := strings.ReplaceAll(i.GetFilename(), "{tag}", tag)
+	if filename == "" {
+		return fmt.Errorf("No download filename provided")
+	}
 	downloadUrl := fmt.Sprintf("https://github.com/%s/releases/download/%s/%s", *opts.Repository, tag, filename)
 	resp, err := http.Get(downloadUrl)
 	if err != nil {
@@ -50,12 +70,35 @@ func (i *GitHubReleaseInstaller) Install() error {
 	}
 	defer resp.Body.Close()
 
-	n, err := io.Copy(out, resp.Body)
+	n, err := io.Copy(tmpOut, resp.Body)
 	if err != nil {
 		return err
 	}
 	if n == 0 {
 		return fmt.Errorf("No data was written to the file")
+	}
+
+	strategy := StrategyNone
+
+	if opts.Strategy != nil {
+		strategy = *opts.Strategy
+	}
+
+	success := false
+
+	switch strategy {
+	case StrategyTar:
+		success, err = i.RunCmdGetSuccess("tar", "-xvf", tmpOut.Name(), "-C", *opts.Destination)
+	case StrategyZip:
+		success, err = i.RunCmdGetSuccess("unzip", tmpOut.Name(), "-d", *opts.Destination)
+	default:
+		io.Copy(out, tmpOut)
+		success = true
+		err = nil
+	}
+
+	if !success {
+		return errors.Join(fmt.Errorf("Failed to extract the downloaded file"), err)
 	}
 
 	return nil
@@ -119,6 +162,10 @@ func (i *GitHubReleaseInstaller) GetOpts() *GitHubReleaseOpts {
 				Linux:   platformDownloadFilenames["linux"],
 				Windows: platformDownloadFilenames["windows"],
 			}
+		}
+		if strategy, ok := (*info.Opts)["strategy"].(string); ok {
+			strat := GitHubReleaseInstallStrategy(strings.ToLower(strategy))
+			opts.Strategy = &strat
 		}
 	}
 	return opts
